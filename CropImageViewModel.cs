d579f3cc7968fea7441fa1f831db966b5def2cdd
Jason <jarnoff14@comcast.net>
2017-03-05 19:24:48 -0500

    My Contributions to Bewander - Initial commit
    My contributions to Bewander social media site:
    
    Inbox.cshtml - entire view file
    
    InboxViewModel.cshtml - entire viewmodel file
    
    _MessagesNave.cshtml - functionality for getting to Inbox view
    
    UsersController.cs - ActionLink for Inbox - get information needed from
    databases to send through to Inbox view
    
    UsersController.cs - ActionLink for CropImage - get information needed
    to send through to CropImage view
    
    UsersController.cs - PartialViewResult - get information needed to send
    through to profile pic lightbox modal
    
    CropImage.cshtml - entire view file
    
    CropImageViewModel.cs - entire viewmodel file
    
    _ProfilePicLightBox.cshtml - entire view
    
    ProfilePage.cshtml - button for changing profile picture and access to
    photo light box modal

---
+++ CropImageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Bewander.DAL;
+using Bewander.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bewander.ViewModels
+{
+    public class CropImageViewModel
+    {
+        public string UserID { get; set; }
+        public string ProfilePicturePath { get; set; }
+        public string CurrentUserID { get; set; }
+        public Image ImageToCrop { get; set; }
+
+        public CropImageViewModel() { }
+
+        public CropImageViewModel(User user, Image profilePicture, string currentUserID, Image imageToCrop)
+        {
+            UserID = user.UserID;
+            ProfilePicturePath = (profilePicture.Path != "no-image.png") ? user.UserID + "/" + profilePicture.Path : profilePicture.Path;
+            CurrentUserID = currentUserID;
+            ImageToCrop = imageToCrop;
+        }
+        
+    }
+
+}
---
+++ InboxViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Bewander.DAL;
+using Bewander.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bewander.ViewModels
+{
+    public class InboxViewModel
+    {
+        public string UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateJoined { get; set; }
+        public string ProfilePicturePath { get; set; }
+        public string ProfilePictureName { get; set; }
+        public string CoverPhotoPath { get; set; }
+        public string CoverPhotoName { get; set; }
+        public string CurrentUserID { get; set; }
+        public RelationshipStatus? RelationshipStatus { get; set; }
+        public int RelationshipID { get; set; }
+        public bool Private { get; set; }
+        public List<Post> Posts { get; set; }
+        public List<Review> Reviews { get; set; }
+        public List<Relationship> Friends { get; set; }
+        public List<Place> Places { get; set; }
+
+        public InboxViewModel() { }
+
+        public InboxViewModel(User user, UserProfile userProfile, Image profilePicture, Image coverPhoto, string currentUserID, RelationshipStatus status, int relationshipID, bool privateProfile, List<Post> posts, List<Review> reviews, List<Relationship> friends, List<Place> places)
+        {
+            UserID = user.UserID;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            DateJoined = user.DateJoined.ToString();
+            ProfilePicturePath = (profilePicture.Path != "no-image.png") ? user.UserID + "/" + profilePicture.Path : profilePicture.Path;
+            ProfilePictureName = profilePicture.FileName;
+            CoverPhotoName = coverPhoto.FileName;
+            CoverPhotoPath = (coverPhoto.Path != "cover-image.jpg") ? user.UserID + "/" + coverPhoto.Path : coverPhoto.Path;
+            CurrentUserID = currentUserID;
+            RelationshipStatus = status;
+            RelationshipID = relationshipID;
+            Private = privateProfile;
+            Posts = posts;
+            Reviews = reviews;
+            Friends = friends;
+            Places = places;
+        }
+
+        public InboxViewModel(User user, UserProfile userProfile, Image profilePicture, Image coverPhoto, string currentUserID)
+        {
+            UserID = user.UserID;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            DateJoined = user.DateJoined.ToString();
+            ProfilePicturePath = (profilePicture.Path != "no-image.png") ? user.UserID + "/" + profilePicture.Path : profilePicture.Path;
+
+            ProfilePictureName = profilePicture.FileName;
+
+            CoverPhotoName = coverPhoto.FileName;
+            CoverPhotoPath = (coverPhoto.Path != "cover-image.jpg") ? user.UserID + "/" + coverPhoto.Path : coverPhoto.Path;
+            CurrentUserID = currentUserID;
+        }
+    }
+}
